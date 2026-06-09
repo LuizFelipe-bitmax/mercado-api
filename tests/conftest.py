18dd0b24d50238726a8b2f621b2666be557ba923
Luiz Felipe <luiz.carmo@icomp.ufam.edu.br>
2026-06-09 01:17:48 -0300

    Corrige usuario de teste para CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,12 +2,32 @@ from fastapi.testclient import TestClient
 
 from main import app
 from database.base import Base
-from database.connection import engine
+from database.connection import engine, SessionLocal
 
 from models.usuario import Usuario
 from models.produtos import Produto
 
+from security.jwt_handler import gerar_hash
+
 Base.metadata.create_all(bind=engine)
+
+db = SessionLocal()
+
+usuario = db.query(Usuario).filter(
+    Usuario.username == "luizfelipe"
+).first()
+
+if not usuario:
+
+    usuario = Usuario(
+        username="luizfelipe",
+        password=gerar_hash("123456")
+    )
+
+    db.add(usuario)
+    db.commit()
+
+db.close()
 
 client = TestClient(app)
 
