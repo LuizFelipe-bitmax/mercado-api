6bb6867cc6dc22ae9cfa752ef3b5b365eaafbab7
Luiz Felipe <luiz.carmo@icomp.ufam.edu.br>
2026-06-09 00:51:02 -0300

    Corrige criação de tabelas nos testes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,13 @@
 from fastapi.testclient import TestClient
 
 from main import app
+from database.base import Base
+from database.connection import engine
+
+from models.usuario import Usuario
+from models.produtos import Produto
+
+Base.metadata.create_all(bind=engine)
 
 client = TestClient(app)
 
